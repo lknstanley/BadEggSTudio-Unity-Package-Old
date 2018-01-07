22faf0d317c370675e7fc429cbabf625eace5f84
Peter Chan <peter@mwh.hk>
2018-01-07 23:23:46 +0800

    temp remove compressor

--- Assets/BadEggStudio Package/BadEggStudio/Utils/Serialize/Compressor.cs
+++ Assets/BadEggStudio Package/BadEggStudio/Utils/Serialize/Compressor.cs
@@ -3,37 +3,48 @@ using System;
 using System.IO;
 using System.IO.Compression;
 
+using UnityEngine ;
+
 namespace BadEggStudio.Utils.Serialize
 {
     public static class Compressor
     {
         public static byte[] Compress( byte[] binary )
         {
-            return _Process( binary, CompressionMode.Compress ) ;
+            // byte[] processed = null ;
+
+            // MemoryStream ms = new MemoryStream() ;
+            // GZipStream gzip = new GZipStream( ms, CompressionMode.Compress ) ;
+            
+            // gzip.Write( binary, 0, binary.Length ) ;
+            // gzip.Flush() ;
+
+            // processed = new byte[ ms.Length ] ;
+            // Buffer.BlockCopy( ms.GetBuffer(), 0, processed, 0, processed.Length ) ;
+
+            // gzip.Close() ;
+            // ms.Close() ;
+
+            // return processed ;
+
+            return binary ;
         }
 
         public static byte[] Decompress( byte[] compressed )
         {
-            return _Process( compressed, CompressionMode.Decompress ) ;
-        }
+            // byte[] buffer = new byte[ 4096 ] ;
+            // MemoryStream inStream = new MemoryStream( compressed ) ;
+            // MemoryStream outStream = new MemoryStream() ;
+            // GZipStream gzip = new GZipStream( inStream, CompressionMode.Decompress ) ;
 
-        private static byte[] _Process( byte[] data, CompressionMode mode )
-        {
-            byte[] processed = null ;
+            // outStream.Flush() ;
 
-            MemoryStream ms = new MemoryStream() ;
-            GZipStream gzip = new GZipStream( ms, mode ) ;
-            
-            gzip.Write( data, 0, data.Length ) ;
-            gzip.Close() ;
+            // buffer = new byte[ outStream.Length ] ;
+            // Buffer.BlockCopy( outStream.GetBuffer(), 0, buffer, 0, buffer.Length ) ;
 
-            processed = new byte[ ms.Length ] ;
-            Buffer.BlockCopy( ms.GetBuffer(), 0, processed, 0, processed
-            .Length ) ;
+            // return buffer ;
 
-            ms.Close() ;
-
-            return processed ;
+            return compressed ;
         }
     }
 }
